205eced36d9bcfb833ac4f4577792d77d53da62d
Darlysson Alves <drlssnlvs@gmail.com>
2021-03-25 14:14:08 -0300

    (feat): list transactions and balances

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,38 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(): Promise<{transactions: Transaction[], balance: Balance}> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, current) => {
+        switch (current.type) {
+          case 'income':
+            accumulator.income += current.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += current.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    return {
+      transactions,
+      balance: {
+        income,
+        outcome,
+        total: income - outcome
+      }
+    }
   }
 }
 
